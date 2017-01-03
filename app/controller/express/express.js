f24741555f290e1dfa13ce703c583f2c7ea0ab9c
phamhongphuc <phamhongphuc.info@gmail.com>
2017-01-03 13:09:13 +0000

    update

--- app/controller/express/express.js
+++ app/controller/express/express.js
@@ -1,6 +1,7 @@
 module.exports = (function () {
     app.get('/'      , require("./express.routes.js")["/"      ]);
     app.get('/info'  , require("./express.routes.js")["/info"  ]);
+    app.get('/report'  , require("./express.routes.js")["/report"  ]);
     
     app.get('/js'    , require("./express.routes.js")["/js"    ]);
     app.get('/min-js', require("./express.routes.js")["/min-js"]);
